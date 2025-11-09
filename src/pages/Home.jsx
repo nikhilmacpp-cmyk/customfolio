ed26c0b9d9d98e74554a5e3a527895c82bd29486
Nikhil PP <nikhilpp@Nikhils-MacBook-Air.local>
2025-11-09 14:28:32 +0530

    Initial commit - copied from react-portfolio

---
+++ src/pages/Home.jsx
@@ -0,0 +1,36 @@
+import { AboutSection } from "../components/AboutSection"
+import { ContactSection } from "../components/ContactSection"
+import { Footer } from "../components/Footer"
+import { HeroSection } from "../components/HeroSection"
+import { Navbar } from "../components/Navbar"
+import { ProjectSection } from "../components/ProjectSection"
+import { SkillsSection } from "../components/SkillsSection"
+import { StartBackGround } from "../components/StartBackGround"
+import { ThemeToggle } from "../components/ThemeToggle"
+
+export const Home = () => {
+  return (
+    <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+      {/* Theme toggle */}
+      <ThemeToggle />
+
+      {/* BackGround Effects */}
+      <StartBackGround />
+
+      {/* Nav bar */}
+      <Navbar />
+
+      {/* Main Content */}
+      <main>
+        <HeroSection />
+        <AboutSection />
+        <SkillsSection />
+        <ProjectSection />
+        <ContactSection />
+      </main>
+
+      {/* Simple Footer */}
+      <Footer />
+    </div>
+  )
+}
